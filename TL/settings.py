845c773266038014089cf84eef85081aa6e6a6af
felipe.melo <felipe.melo@fdte.io>
2021-01-27 16:33:29 -0300

    :truck: Rename app

--- TL/settings.py
+++ TL/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pagamento.apps.PagamentoConfig',
+    'lanchonete.apps.LanchoneteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
