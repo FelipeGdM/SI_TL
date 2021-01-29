644998daffe09fdbe7ce5a0d2c6c6d4ce0cc4bcc
kawai <juliano.fonseca@thunderatz.org>
2021-01-29 15:11:44 -0300

    Fix merge

--- lanchonete/urls.py
+++ lanchonete/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('estoque', views.estoque, name='estoque'),
-    path('inventario', views.inventario, name='inventario')
+    path('inventario', views.inventario, name='inventario'),
     path('signin', views.signin, name='signin'),
     path('homeuser',views.homeuser, name='homeuser'),
     path('pagamento',views.pagamento, name='pagamento'),
-    path('carrinho',views.carrinho, name='carrinho')
+    path('carrinho',views.carrinho, name='carrinho'),
 ]
